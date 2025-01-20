916952e666d2759f29a40e4677abc18c1b86fd19
Carlos Moragon Corella <cmoragoncorella@gmail.com>
2025-01-20 18:41:14 +0100

    Cambiado de archivo .ipynb a modulos

--- src/core/feature_extraction.py
+++ src/core/feature_extraction.py
@@ -17,3 +17,45 @@ def get_posible_families(experiment_data):
 
 def filter_by_family(experiment_data, families, family_name, family):
     return experiment_data[[family_name in conjunto or family in conjunto for conjunto in families]]
+
+def filter_by_rt(datasets):
+    # Ordenar datasets por RT
+    for dataset in datasets:
+        dataset.sort_values(by="rt", inplace=True)
+        dataset = dataset[dataset['rt'] > dataset['column.t0']]
+    
+    return datasets
+
+# Filtra el dataset según su configuración 
+def filter_by_config(data_family, config):
+    usp_columns = [
+        'column.usp.code_0', 'column.usp.code_L1', 'column.usp.code_L10',
+        'column.usp.code_L109', 'column.usp.code_L11', 'column.usp.code_L114',
+        'column.usp.code_L122', 'column.usp.code_L3', 'column.usp.code_L43',
+        'column.usp.code_L68', 'column.usp.code_L7'
+    ]
+    # Crear una lista de condiciones para las columnas usp_code
+    usp_conditions = [
+        (data_family[usp_col] == 1) if usp_col == config.columna.usp_code else (data_family[usp_col] == 0)
+        for usp_col in usp_columns
+    ]
+
+    # Combinar todas las condiciones usando '&'
+    combined_usp_condition = usp_conditions[0]
+    for condition in usp_conditions[1:]:
+        combined_usp_condition &= condition
+
+    # Crear la condición general
+    combined_condition = (
+        (data_family['column.name'] == config.columna.name) &
+        (data_family['column.length'] == config.columna.length) &
+        (data_family['column.id'] == config.columna.id) &
+        (data_family['column.particle.size'] == config.columna.particle_size) &
+        (data_family['column.temperature'] == config.columna.temperature) &
+        (data_family['column.flowrate'] == config.columna.flowrate) &
+        (data_family['column.t0'] == config.columna.t0) &
+        combined_usp_condition  # Condiciones combinadas de usp_code
+    )
+
+    # Filtrar los datos que cumplen la condición
+    return data_family[combined_condition]
--- src/core/processing.py
+++ src/core/processing.py
@@ -1,4 +1,6 @@
 from models.models import Column, Config
+from core.feature_extraction import filter_by_family, get_posible_families, filter_by_config
+from core.calculations import alpha, diff
 
 def create_config_objects(dataset):
     def get_uspcode(row):
@@ -31,3 +33,64 @@ def create_config_objects(dataset):
         config_objects.add(config)
 
     return list(config_objects)
+
+# Dada los tipos de experimentos separa
+def process_experiments(data, experiments, family_name, family):
+    result_datasets = []
+    configs = []  # Lista de configuraciones
+    for exp in experiments:
+        experiment_data = data[data['experiment'] == exp]
+        families = get_posible_families(experiment_data)
+        data_family = filter_by_family(experiment_data, families, family_name, family)
+            
+        # Creamos un objeto configuración y lo añadimos a una lista
+        exp_configs = create_config_objects(data_family)
+        configs.extend(exp_configs)
+            
+        datasets_by_config = [filter_by_config(data_family, config) for config in exp_configs]
+        result_datasets.extend(datasets_by_config)
+        
+    return result_datasets, configs
+
+def calculate_alpha_results(dataset):
+    resultados_dataset = []
+    primer_elemento = dataset.iloc[0]
+    #t0_value = t0(primer_elemento['column.length'], primer_elemento['column.flowrate'])
+        
+    for i in range(1, len(dataset)):
+        elemento_n_menos_1 = dataset.iloc[i - 1]
+        elemento_n = dataset.iloc[i]
+        resultado = alpha(elemento_n_menos_1['rt'], elemento_n['rt'], elemento_n_menos_1['column.t0'], elemento_n['column.t0'])#t0_value)
+        resultados_dataset.append(resultado)
+    return resultados_dataset
+
+def calculate_diff_results(dataset):
+    resultados_dataset = []
+    ultimo_elemento = dataset.iloc[len(dataset)-1]
+    total = ultimo_elemento['rt']
+        
+    for i in range(1, len(dataset)):
+        elemento_n_menos_1 = dataset.iloc[i - 1]
+        elemento_n = dataset.iloc[i]
+        resultado = diff(elemento_n_menos_1['rt'], elemento_n['rt'], total)
+        resultados_dataset.append(resultado)
+    return resultados_dataset
+
+def calculate_results(result_datasets, is_alpha):
+    resultados = []
+    for dataset in result_datasets:
+        if dataset.empty:
+            continue
+        if(is_alpha):
+            resultados.append(calculate_alpha_results(dataset))
+        else:
+            resultados.append(calculate_diff_results(dataset))
+    return resultados
+
+def build_results_list(configs, resultados, fscore):
+    lista_tuplas = []
+    for i in range(min(len(configs), len(resultados))):
+        # Aplicamos la función fscore a los resultados de cada configuración
+        score = fscore(resultados[i])
+        lista_tuplas.append((configs[i].__str__(), score.__str__()))  # Emparejamos config con su puntaje
+    return lista_tuplas
