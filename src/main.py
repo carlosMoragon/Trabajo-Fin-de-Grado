b36b896103e9220543e8897a6057f1cec1bb7483
Carlos Moragon Corella <cmoragoncorella@gmail.com>
2025-01-20 18:41:14 +0100

    Cambiado de archivo .ipynb a modulos

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from core.data_processing import cargar_dataset, preprocesar_dataset
-from core.feature_extraction import get_types_of_experiments, get_family_name, filter_by_family, get_posible_families
-from core.calculations import fscore_mejor_caso
-from core.processing import create_config_objects
+from core.feature_extraction import get_types_of_experiments, get_family_name, filter_by_rt
+from core.calculations import fscore_mejor_caso, fscore_peor_caso, fscore_caso_medio
+from core.processing import process_experiments, calculate_results, build_results_list
 
 import config
 import argparse
@@ -9,13 +9,21 @@ import argparse
 def main():
 
     # Crear un parser para recibir los argumentos desde la terminal
-    parser = argparse.ArgumentParser(description="(Obligatorio) Introduce la clase a evaluar como primer parámetro.\n(No obligatorio) Si deseas puedes especificar el modo de evaluación: 'alpha' o 'diff'")
+    parser = argparse.ArgumentParser(description="(Obligatorio) Introduce la clase a evaluar como primer parámetro.\n(No obligatorio) Especifica en caso lo quieres evaluar: peor, medio o mejor.\n(No obligatorio) Si deseas puedes especificar el modo de evaluación: 'alpha' o 'diff'")
 
     # Hay que quitar el nargs y el default de esta linea cuando se entre en producción
     parser.add_argument('family', help="Introduce la clase a evaluar", nargs='?', default='Organooxygen compounds (CHEMONTID:0000323)')
+    parser.add_argument('eval', help="Evaluar en el 'mejor' caso, caso 'medio' o 'peor' caso?", nargs='?', default='peor')
     parser.add_argument('modo', choices=['alpha', 'diff'], help="El segundo argumento (opcional)", nargs='?', default=('alpha' if config.IS_ALPHA else 'diff'))
 
     args = parser.parse_args()
+
+    if args.eval == 'mejor':
+        fscore = fscore_mejor_caso
+    elif args.eval == 'peor':
+        fscore = fscore_peor_caso
+    elif args.eval == 'medio':
+        fscore = fscore_caso_medio
 
     if args.modo == 'alpha':
         is_alpha = True
@@ -34,66 +42,21 @@ def main():
     types_of_experiments = get_types_of_experiments(dataset)
     family_name = get_family_name(family)
 
-    result_datasets = []
-    configs = []
-    for exp in types_of_experiments:
-        experiment_data = dataset[dataset['experiment'] == exp]
-        families = get_posible_families(experiment_data)
-        data_family = filter_by_family(experiment_data, families, family_name, family)
-        config_objects = create_config_objects(data_family)
-        configs.extend(config_objects)
-            
-        datasets_by_config = [filter_by_config(data_family, config) for config in exp_configs]
-        result_datasets.extend(datasets_by_config)
+    result_datasets, configs = process_experiments(dataset, types_of_experiments, family_name, family)
 
-        print(f"Experiment {exp}: {config_objects}")
-
-
-def process_experiments(data, experiments, family_name, family):
-    result_datasets = []
-    configs = []  # Lista de configuraciones
-    for exp in experiments:
-        experiment_data = data[data['experiment'] == exp]
-        families = get_posible_families(experiment_data)
-        data_family = filter_by_family(experiment_data, families, family_name, family)
-            
-        # Creamos un objeto configuración y lo añadimos a una lista
-        exp_configs = create_config_objects(data_family)
-        configs.extend(exp_configs)
-            
-        datasets_by_config = [filter_by_config(data_family, config) for config in exp_configs]
-        result_datasets.extend(datasets_by_config)
-        
-    return result_datasets, configs
-
-def main():    
-
-    # Cargamos el dataset y lo preprocesamos
-    dataset = cargar_dataset(config.DATA_PATH, config.TSV_NAME)
-    dataset = preprocesar_dataset(dataset)
-
-    # Obtener los tipos de experimentos y el nombre de la familia
-    types_of_experiments = get_types_of_experiments(dataset)
-    family_name = get_family_name(family)
-
-    # A PARTIR DE AQUI ME PIERDO *1
-
-    # Procesar experimentos y obtener datasets y configuraciones
-    result_datasets, configs = process_experiments(data, types_of_experiments, family_name, family)
-
-    #save_excel(result_datasets, "../data/result_datasets.xlsx")
-
-    # Ordenar datasets por RT
-    for dataset in result_datasets:
-        dataset.sort_values(by="rt", inplace=True)
+    datasets = filter_by_rt(result_datasets)
 
     # Calcular resultados de alpha
-    resultados = calculate_results(result_datasets, is_alpha=True)
+    resultados = calculate_results(datasets, is_alpha=is_alpha)
 
     # Construir la lista de resultados finales
     lista_tuplas = build_results_list(configs, resultados, fscore)
 
+    print([tupla[1] for tupla in lista_tuplas])
+
     return lista_tuplas
+    
+
 
 if __name__ == "__main__":
     main()
