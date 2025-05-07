747a09f01dc856d290f4ed4a39d4d81472aa25a8
Carlos Moragon Corella <cmoragoncorella@gmail.com>
2025-05-07 19:43:59 +0200

    BBDD para desplegar

--- API_REST/metabolite-separation-api/index.js
+++ API_REST/metabolite-separation-api/index.js
@@ -5,20 +5,23 @@ const mongoose = require('mongoose');
 
 const launchServer = async () => {
   try {
-    // Conectar a MongoDB (sin opciones deprecadas)
-    await mongoose.connect('mongodb://localhost:27017/metabolite-separation-api');
-    console.log('✅ MongoDB connected');
+    require('dotenv').config(); 
+    const host = process.env.DATABASE_HOST;
+    const port = process.env.DATABASE_PORT;
+    const name = process.env.DATABASE_NAME;
+    const user = process.env.DATABASE_USER || 'root';
+    const password = process.env.DATABASE_PASSWORD || 'rootpassword';
 
-    // Iniciar servidor Express
+    await mongoose.connect(`mongodb://${user}:${password}@${host}:${port}/${name}?authSource=admin`);
+    console.log('✅ MongoDB connected');
+    
     const expressServer = new ExpressServer(config.URL_PORT, config.OPENAPI_YAML);
     expressServer.launch();
     logger.info('Express server running');
   } catch (error) {
-    logger.error('Express Server failure', error.message);
+    logger.error('Express Server failure', error.message, error.stack);
     await mongoose.connection.close();
   }
 };
 
 launchServer().catch(e => logger.error(e));
-
-
