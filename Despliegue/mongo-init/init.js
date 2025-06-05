cf985f2c80c06e5d34bab83b621e2be91b19cba8
Carlos Moragon Corella <cmoragoncorella@gmail.com>
2025-06-05 17:49:48 +0200

    REESTRUCTURADO

--- Despliegue/mongo-init/init.js
+++
@@ -1,8 +0,0 @@
-db = db.getSiblingDB('metabolite-separation-api');
-
-db.createCollection('evaluates');
-db.createCollection('experiments');
-db.createCollection('families');
-db.createCollection('feedbacks');
-db.createCollection('predicts');
-db.createCollection('recommendfamilies');
